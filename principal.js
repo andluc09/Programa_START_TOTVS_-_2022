97386a8faa24ac1d184a2e490a3ca1f18b2749ac
André Lucas de Macedo Santos <70043466+andluc09@users.noreply.github.com>
2022-01-29 23:43:46 -0300

    Adicionando os arquivos do Portfólio.

---
+++ principal.js
@@ -0,0 +1,19 @@
+!function(){ 
+  
+/*function x() {
+ document.getElementById("id").style.position = "x";
+}*/
+
+//var timeoutID = setTimeout((document.getElementById("mensagem")), 5000);
+
+/*setTimeout('bemVindo()',9000);
+function bemVindo(){
+alert("Conectando . . . \n \n Aguarde!!");
+}*/
+
+}
+  
+
+
+
+
